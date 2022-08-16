00aac3124aba1c580efa255515740c5fed265382
EJKonge <ejkonge@gmail.com>
2022-08-16 23:02:36 +0100

    added get_computer_choice

--- camera_rps.py
+++ camera_rps.py
@@ -6,8 +6,8 @@ import time
 
 class RPS:
 
-     def __init__(self):
-        #self.computer_choice = random.choice(self.choice_list)
+    def __init__(self):
+        self.computer_choice = random.choice(self.choice_list)
         self.model = load_model('keras_model.h5')
         self.cap = cv2.VideoCapture(0)
         self.data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
@@ -15,43 +15,10 @@ class RPS:
         self.computer_score = 0
         pass
 
-    
-    start_time = time.time()
-    print("You have 5 seconds to choose.")
-
-    while True:
-
-        elapsed_time = time.time() - start_time
-        remaining_time = 5 - elapsed_time
-        #print("You have {:.0f} seconds left".format(remaining_time))
-        
-        ret, frame = cap.read()
-        resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
-        image_np = np.array(resized_frame)
-        normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
-        data[0] = normalized_image
-        prediction = model.predict(data)
-        cv2.imshow('frame', frame)
-        # Press q to close the window
-
-        if (remaining_time < 0):
-            user_choice = get_prediction(prediction)
-            computer_choice = get_computer_choice()
-            print("You chose: {}".format(user_choice))
-            print("Computer choice was: {}".format(computer_choice))
-            get_winner(computer_choice, user_choice)
-
-            print("You have 5 seconds to choose.")
-            start_time = time.time()
-            remaining_time = 5
-        
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-                
-    # After the loop release the cap object
-    cap.release()
-    # Destroy all the windows
-    cv2.destroyAllWindows()
+    def get_computer_choice(self):
+        choice_list = ['Rock','Paper','Scissors']
+        computer_choice = random.choice(choice_list).lower()
+        return computer_choice
 
     def get_prediction(self):
         
@@ -66,3 +33,44 @@ class RPS:
         else:
             user_choice = 'No user choice detected'
         return user_choice
+    
+    def timer(self):
+
+        start_time = time.time()
+        print("You have 5 seconds to choose.")
+
+        while True:
+
+            elapsed_time = time.time() - start_time
+            remaining_time = 5 - elapsed_time
+            #print("You have {:.0f} seconds left".format(remaining_time))
+            
+            ret, frame = self.cap.read()
+            resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
+            image_np = np.array(resized_frame)
+            normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
+            self.data[0] = normalized_image
+            prediction = self.model.predict(self.data)
+            cv2.imshow('frame', frame)
+            # Press q to close the window
+
+            if (remaining_time < 0):
+                user_choice = self.get_prediction(prediction)
+                computer_choice = self.get_computer_choice()
+                print("You chose: {}".format(user_choice))
+                print("Computer choice was: {}".format(computer_choice))
+                self.get_winner(computer_choice, user_choice)
+
+                print("You have 5 seconds to choose.")
+                start_time = time.time()
+                remaining_time = 5
+            
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+                    
+        # After the loop release the cap object
+        self.cap.release()
+        # Destroy all the windows
+        cv2.destroyAllWindows()
+
+    
